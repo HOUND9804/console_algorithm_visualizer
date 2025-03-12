c9618cbc571a1c0ff158b51cc01263c32059ed77
Taseen Tajwar <taseentajwar95@gmail.com>
2025-03-12 15:43:50 +0600

    Updated MyQueue.h

--- MyQueue.h
+++ MyQueue.h
@@ -1,20 +1,30 @@
 #ifndef MYQUEUE_H
 #define MYQUEUE_H
 
-#include <deque>
+#include <vector>
 #include <iostream>
 
 template <typename T>
 class MyQueue {
 private:
-    std::deque<T> queue;  // Underlying container for the queue
+    std::vector<T> queue;  // Using vector instead of deque
 
 public:
     MyQueue() = default;
+
+    // Enqueue an element at the back of the queue
     void enqueue(const T& value);
+
+    // Dequeue an element from the front of the queue
     void dequeue();
+
+    // Get the front element of the queue
     T front() const;
+
+    // Check if the queue is empty
     bool isEmpty() const;
+
+    // Get the size of the queue
     size_t size() const;
 };
 
